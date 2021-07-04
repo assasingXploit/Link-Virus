55f3552b70762838912bd364892ee462f89b6fbf
★Mr.ASSASING.777★ <86616324+assasingXploit@users.noreply.github.com>
2021-07-05 03:54:58 +0800

    Add files via upload

---
+++ Virus.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+#by Mr.ASSASING
+#Silahkan Di Recode ANJNG
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+clear
+sleep 2
+cowsay -f eyes "Hacked By Mr.ASSASING
+
+TEAM : MINIONS CYBER CRIME" | lolcat
+neofetch
+echo
+    sleep 2
+    echo $cyan":::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+    echo $cyan"::"$yellow"          AUTHOR"$cyan" :"$yellow" Mr.ASSASING.7"$cyan"★"
+    echo $cyan"::"$yellow"          TEAM  "$cyan" :"$yellow" MINIONS CYBER CRIME"$cyan"★"
+    echo $cyan"::"$yellow"          IG    "$cyan" :"$yellow" mr.assasing.777"$cyan"★"
+    echo $cyan":::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+    echo
+sleep 1
+echo $purple" GUNAKAN TOOLS INI DENGAN BIJAK"
+echo $cyan"°==============================°"
+echo
+         sleep 1
+         echo $cyan"["$yellow"01"$cyan"]"$purple" LINK VIRUS KE SATU"
+         echo $cyan"["$yellow"02"$cyan"]"$purple" LINK VIRUS KE DUA"
+         echo $cyan"["$yellow"03"$cyan"]"$purple" KELUAR"
+         echo
+         sleep 1
+echo $cyan":::::::::::::::::::::::"
+echo $cyan"::"$yellow" MASUKAN PILIHANMU"$cyan" ::"
+echo $cyan":::::::::::::::::::::::"
+echo
+sleep 1
+read -p "=> " gady
+
+if [ $gady = 1 ] || [ $gady = 01 ]
+then
+    sleep 1
+    echo $red"SEDANG"$yellow" MENUJU"$green" KE"$blue" LINK"
+    sleep 3
+    sh LiNk1.sh
+fi
+if [ $gady = 2 ] || [ $gady = 02 ]
+then
+    sllep 1
+    echo $red"SEDANG"$yellow" MENUJU"$green" KE"$blue" LINK"
+    sleep 3
+    sh LiNk2.sh
+fi
+if [ $gady = 3 ] || [ $gady = 03 ]
+then
+    sleep 1
+    echo $yellow"TERIMAKASIH TELAH MENGGUNAKAN SCRIPT SAYA"
+    sleep 1
+    echo $cyan"SALAM Mr.ASSASING"
+        exit
+fi
